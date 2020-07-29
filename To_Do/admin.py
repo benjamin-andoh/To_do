287901db6dacc1b742fbee5256c78d874081ac56
benjamin-andoh <benoo-andoh@gmail.com>
2020-07-29 14:36:54 +0000

    signals modified

--- To_Do/admin.py
+++ To_Do/admin.py
@@ -6,7 +6,7 @@ class TaskAdmin(admin.ModelAdmin):
     list_display = ('supervisor', 'title', 'category', 'description', 'task_done', 'start_date', 'end_date', 'developer')
     search_fields = ('developer__username', 'supervisor__username','title',)
     list_filter = ('task_done', 'category', )
-    # list_editable = ('task_done',)
+    list_editable = ('task_done',)
 
     # def get_ordering(self, request):
     #     if request.user.is_superuser:
--- To_Do/models.py
+++ To_Do/models.py
@@ -1,34 +1,41 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
-
+from django.core.exceptions import ObjectDoesNotExist
 
 # user, title, category, description, task_done, start_date, end_date
+from django.dispatch import receiver
+
+
 class Task(models.Model):
     cat = (
-        ('programming', 'programming'),
-        ('bugfixing', 'bugfixing'),
-        ('something', 'something'),
+        ('p', 'programming'),
+        ('b', 'bugfixing'),
+        ('s', 'something'),
     )
     title = models.CharField(max_length=100)
     category = models.CharField(max_length=100, choices=cat)
     description = models.TextField(blank=True)
-    task_done = models.BooleanField(default=False, editable=True)
-    start_date = models.DateTimeField(auto_now_add=True, editable=False)
+    task_done = models.BooleanField(default=False)
+    start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField(null=True, editable=False)
-    developer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
-    supervisor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+    developer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='developer')
+    supervisor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='supervisor')
 
     def __str__(self):
-        name = self.title + " " + self.developer.username + " " + str(self.task_done)
+        name = f"{self.title} {self.developer.username}  {self.task_done}"
         return name
 
 
 # i was testing this using the shell
-def send_mail(sender, instance, **kwargs):
-    task = Task()
-    if task.supervisor != task.developer:
-        print("you have a new task")
+def send_mail(sender, *args, **kwargs):
+    task = kwargs['instance']
+    if kwargs['created']:
+        if task.supervisor != task.developer:
+            print("you have a new task")
 
 
 post_save.connect(send_mail, sender=Task)
+
+
+
